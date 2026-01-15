2942455cc3a6a5e50868eef7dddb9a7a9a5f8c4a
ArdaKe26 <arda.kurtulus26@gmail.com>
2026-01-15 10:03:19 +0300

    Update LimelightLocalization.java
    Fixed issue in LimelightHelpers.java

--- src/main/java/frc/robot/vision/LimelightLocalization.java
+++ src/main/java/frc/robot/vision/LimelightLocalization.java
@@ -11,7 +11,7 @@ public class LimelightLocalization extends SubsystemBase {
     String limelightName;
     int[] validIDs = {
         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-        17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,31
+        17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
     };
 
     double last_yaw = 0; double last_yaw_velocity = 0;

b55ec344a05a7e5e9b711cf92589f25a847b1c34
SortaHappyDoge <ocalakif1@gmail.com>
2026-01-18 18:02:06 +0300

    mend

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,10 @@ package frc.robot;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.path.PathConstraints;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
 public final class Constants {
     public static final double kRobotMaxSpeed = 3; // In meters per second
     //public static final double kRobotMaxSpeedAutonomous = 3; // In meters per second
@@ -20,6 +24,20 @@ public final class Constants {
     public static final PathConstraints kPathfindingConstraints = new PathConstraints(
       kRobotMaxSpeed, kRobotMaxAcceleration,
       kRobotMaxAngularSpeed, kRobotMaxAngularAcceleration);
+
+
+    /**
+     * @WARNING THESE ARE TEMPORARY VALUES MADE FOR TESTING
+     */
+    public static final Translation2d kShooterOffset = new Translation2d(-1,1); // In meters
+    public static final double kShooterOffsetDistance = Math.sqrt(Math.pow(kShooterOffset.getX(), 2)+Math.pow(kShooterOffset.getY(), 2));
+    public static final double kShooterOffsetAngle = Math.atan2(kShooterOffset.getY(), kShooterOffset.getX()); // In radians
+    public static final Translation2d kTargetPosBlue = new Translation2d(5, 4);
+    public static final Translation2d kTargetPosRed = new Translation2d(11, 4);
+    public static final double[] kShootingDistances = {3, 5, 8}; // Precalculated configs based on the distance to the target TODO: Write a better comment
+    public static final double[] kShootingAngles = {80, 70, 60};
+    public static final double[] kShootingRPMs = {5000, 5500, 6000};
+    public static final double[] kAirtimes = {1, 0.8, 0.75};
 
     /**
      * @WARNING these constants MUST be initialized at the start of the robot code.
